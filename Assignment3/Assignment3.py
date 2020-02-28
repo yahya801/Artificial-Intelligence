49d42438d8797ce90ad652949f5ae3c0378f2790
yahya801 <53046561+yahya801@users.noreply.github.com>
2020-02-28 16:57:11 +0500

    Board and population made

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -1,11 +1,40 @@
-from queue import PriorityQueue
 
-def hello():
-    open_list = PriorityQueue()
-    open_list.put(43,'5')
-    open_list.put(42,'6')
+
+import random
+
+
+def boardmaking(size,board):
+   for i in range(size):
+        board.append([0 for i in range(size)])
+       
+
+def tostring():
+    for row in board:
+            for val in row:
+                print(val, end=" ")
+            print()
+
+def populationgeneration():
+    populationsize = random.randint(1,8)
+    population = []
+    print(populationsize)
     
-    print(open_list.get())
-    print("yahya")
+    for i in range(1,populationsize+1):
+        population.append(i)
+    
+    for i in range(len(population)):
+    #   a = population.index()
 
-hello()
+        population[i] = i+1, [random.randint(1,8),random.randint(1,8),random.randint(1,8),random.randint(1,8),random.randint(1,8),random.randint(1,8),random.randint(1,8),random.randint(1,8)]  
+        
+      
+   # population[1] =random.randint(1,8),random.randint(1,8)}
+    print(population)
+    
+
+
+
+board =[]
+boardmaking(8,board)
+tostring()
+populationgeneration()
