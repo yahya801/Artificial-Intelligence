29da227357fc8bb8b47fae744c997dbd78f23a6a
yahya801 <53046561+yahya801@users.noreply.github.com>
2020-03-19 21:45:04 +0500

    Genetic Algorithm Done

--- Assignment3/GeneticAlgo.py
+++ Assignment3/GeneticAlgo.py
@@ -1,6 +1,7 @@
 import random
 
 
+
 def makeboard(board):
 
     board.clear()
@@ -16,6 +17,7 @@ def tostring():
 
 
 def populationgen():
+    population.clear()
     for i in range(0, 8):
         queenpos.append([random.randint(1, 8), random.randint(1, 8), random.randint(1, 8), random.randint(
             1, 8), random.randint(1, 8), random.randint(1, 8), random.randint(1, 8), random.randint(1, 8)])
@@ -25,6 +27,7 @@ def populationgen():
         population.append((fitness, queenpos[i]))
     population.sort()
    # print(population,'generation')
+
 
 def fitnesscalc(array, board):
     # Row Clash
@@ -78,7 +81,7 @@ def fitnesscalc(array, board):
                         clash[i] = clash[i]+1
             row -= 1
             column -= 1
-   
+
     fitness = 0
     for k in range(len(clash)):
         if clash[k] == 0:
@@ -93,20 +96,18 @@ def roulwheel():
     sum = 0
     fitt = 0
    # print(range(len(population)),'range')
-   
+
     for i in range(len(population)):
-        
+
         fit, c = population[i]
         sum += fit
-        if(sum== 0):
-            sum=0.2
+
     for i in range(len(population)):
         fit, chr = population[i]
         fitness = round((fit/sum), 2)
-       
+
         fitt += fitness
         probabilities.append(fitt)
-    
 
     p1 = random.choice(probabilities)
     for i in range(len(probabilities)):
@@ -145,23 +146,19 @@ def mutation(array):
     child = []
     for i in range(0, 2):
         r = random.randint(0, 7)
-       
+
         ranint = bin(array[r])
 
         if (len(ranint) == 3):
             templist = list(ranint)
             templist.append('1')
-            
             ranint = ''.join(templist)
-            
             ranint = int(ranint, 2)
-            
-
         elif (len(ranint) == 4):
             templist = list(ranint)
-           
+
             r = random.randint(2, 3)
-        
+
             if r != 2 or r == 3:
                 if templist[r] == '0':
                     templist[r] = '1'
@@ -170,13 +167,12 @@ def mutation(array):
             else:
                 templist.append('1')
             ranint = ''.join(templist)
-           
+
             ranint = int(ranint, 2)
-           
 
         elif (len(ranint) == 5):
             templist = list(ranint)
-           
+
             if templist[3] == '0':
                 templist[3] = '1'
             else:
@@ -187,13 +183,10 @@ def mutation(array):
                 templist[4] = '0'
 
             ranint = ''.join(templist)
-           
             ranint = int(ranint, 2)
-            
 
         elif (len(ranint) == 6):
             templist = list(ranint)
-           
             if templist[2] == '1':
                 templist[2] = '0'
                 a = random.randint(3, 5)
@@ -203,13 +196,9 @@ def mutation(array):
                 if templist[b] == '0':
                     templist[b] = '1'
             ranint = ''.join(templist)
-            
             ranint = int(ranint, 2)
-       
-     
-
         array[r] = ranint
-   
+
     child = (array)
     return child
 
@@ -219,61 +208,70 @@ def fillboard(array):
         for j in range(8):
             if i+1 == array[j]:
                 board[i][j] = 'F'
-           
 
 
 def geneticalgo():
     populationgen()
-    chromosome=[]
-    fit= 0
+    chromosome = []
+    fit = 0
     i = 0
-    while (fit < 4):
-        if (len(population)> 75):
-            population.pop()
-        print('iteration =',i)
-        
+    a = 0
+    while (fit <= 7):
+        if (len(population) > 100):
+           
+            population.pop(0)
+
+       
         roulwheel()
         offspring1, offspring2 = crossover()
-      #  print(offspring1, offspring2,'offsprings')
+
         newpop1 = mutation(offspring1)
         newpop2 = mutation(offspring2)
         makeboard(board)
         fillboard(newpop1)
         fitness = fitnesscalc(newpop1, board)
-       # print(fitness)
-        offspring = (fitness,newpop1)
+        offspring = (fitness, newpop1)
         makeboard(board)
         fillboard(offspring2)
         fitness2 = fitnesscalc(newpop2, board)
-       # print(fitness2)
         if(fitness > fitness2):
-          #  print('helllo')
             population.append(offspring)
             fit = fitness
         else:
-            offspring = (fitness2,newpop2)
+            offspring = (fitness2, newpop2)
             population.append(offspring)
             fit = fitness2
 
-        a,chromosome = offspring
+        a, chromosome = offspring
+        print('iteration =', i, 'chromosome: ', chromosome, 'fitness =', fit)
        # print(fit)
         population.sort()
-        i+=1
-       
+
+        i += 1
+        a += 1
+        if (a > 2500):
+            populationgen()
+            a = 0
+
+        if (i > 25000):  # to limit no of iterations
+            if (fit >= 6):
+                break
 
     
-    # newpop2 = mutation(offspring2)
-    # population.append(newpop2)
     print(chromosome)
-  #  print(population)
+    #print(population)
 
 
+
+
+
+# Variables
 population = []
 parents = []
 probabilities = []
 board = []
 queenpos = []
-# makeboard(board)
-# tostring()
+
+# Function Call
 
 geneticalgo()
